e3ee2b656cfe8178536eaae7710e620d7881a49e
max-baum <miba2020@mymail.pomona.edu>
2025-04-11 10:08:34 -0700

    push

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -19,7 +19,7 @@ echo '==========================================================================
 time for file in $files; do
     echo
     # copy your solution to the twitter_postgres assignment here
-    python3 load_tweets.py --db=postgresql://postgres:pass@localhost:6869/postgres --inputs="$f    ile"
+    python3 load_tweets.py --db=postgresql://postgres:pass@localhost:6869/postgres --inputs="file"
 done
 
 echo '================================================================================'
